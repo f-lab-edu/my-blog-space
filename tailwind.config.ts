e73c87cf7f9a9ff80c05c7becfa800fd26d07daa
minji Kong <minzyaaaaaa@gmail.com>
2024-07-21 16:36:24 +0900

    Chore: add Daisy UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,42 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        roboto: ['var(--font-roboto)'],
+        robotoSerif: ['var(--font-roboto-serif)'],
+        notoSans: ['var(--font-noto-sans)'],
+      },
+      fontSize: {
+        '4xxl': [
+          '40px',
+          {
+            lineHeight: '1',
+          },
+        ],
+      },
+      width: {
+        54: '12.5rem', // 200px
+        content: '75rem', // 1200px
+      },
+      margin: {
+        auto: '0 auto',
+      },
+      gap: {
+        '17': '4.25rem', // 68px
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['wireframe', 'black'],
+    darkMode: 'black',
+  },
 };
 export default config;
