44dacc802fb4482c245fa5d69b314fd87f1c9fd5
minji Kong <minzyaaaaaa@gmail.com>
2024-08-04 20:35:22 +0900

    Feat: #5 블로그 리스트 페이지 구현

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,13 +1,30 @@
-'use client';
+import Image from 'next/image';
+import { fetchBlogList } from '@/features/blog/apis';
 
-import React from 'react';
-import { useSearchParams } from 'next/navigation';
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { category?: string; tag?: string };
+}) {
+  const { tag, category } = searchParams;
+  const { contents } = await fetchBlogList({ tag, category });
 
-export default function Page() {
-  const searchParams = useSearchParams();
-
-  const tag = searchParams.get('tag');
-  const category = searchParams.get('category');
-
-  return <div>BLOG</div>;
+  return (
+    <div>
+      {contents.map((data: any) => (
+        <div key={data.id} className='flex justify-between cursor-pointer'>
+          <div>
+            <p className='font-bold'>{data.title}</p>
+            <p>{data.content}</p>
+            <p className='font-light text-xs'>{data.date}</p>
+          </div>
+          <div className='relative w-16 h-16 bg-slate-100'>
+            <Image alt='blog-thumbnail' src={data.thumbnail} fill={true} />
+          </div>
+        </div>
+      ))}
+      <div>{searchParams.category}</div>
+      <div>{searchParams.tag}</div>
+    </div>
+  );
 }
