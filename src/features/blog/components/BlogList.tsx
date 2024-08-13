8ba44ee5975becd67e025dd15be0e1b415ee9169
minji Kong <minzyaaaaaa@gmail.com>
2024-08-14 01:51:28 +0900

    Feat: #6 블로그 리스트에서 상세 페이지 연결, 라우트 추가

--- src/features/blog/components/BlogList.tsx
+++ src/features/blog/components/BlogList.tsx
@@ -1,27 +1,31 @@
+import Link from 'next/link';
 import Image from 'next/image';
 
+import { ROUTES } from '@/shared/config/routes';
 import { Blog } from '@/entities/blog/types';
 
 export default function BlogList({ contents = [] }: { contents: Blog[] }) {
   return (
     <ul className='flex flex-col gap-5'>
-      {contents.map((data: any) => (
-        <li key={data.id} className='flex justify-between cursor-pointer'>
-          <div>
-            <p className='font-bold'>{data.title}</p>
-            <p>{data.content}</p>
-            <p className='font-light text-xs'>{data.date}</p>
-          </div>
-          <div className='relative w-16 h-16 bg-slate-100 border border-black'>
-            <Image
-              alt='blog-thumbnail'
-              src={data.thumbnail}
-              fill
-              sizes='100%, 100%'
-              style={{ objectFit: 'cover' }}
-            />
-          </div>
-        </li>
+      {contents.map((data) => (
+        <Link key={data.id} href={ROUTES.BLOG.DETAIL(data.slug)}>
+          <li key={data.id} className='flex justify-between cursor-pointer'>
+            <div>
+              <p className='font-bold'>{data.title}</p>
+              <p>{data.content}</p>
+              <p className='font-light text-xs'>{data.date}</p>
+            </div>
+            <div className='relative w-16 h-16 bg-slate-100 border border-black'>
+              <Image
+                alt='blog-thumbnail'
+                src={data.thumbnail}
+                fill
+                sizes='100%, 100%'
+                style={{ objectFit: 'cover' }}
+              />
+            </div>
+          </li>
+        </Link>
       ))}
     </ul>
   );
--- src/mocks/handlers/blog.ts
+++ src/mocks/handlers/blog.ts
@@ -6,6 +6,7 @@ const getBlogList = http.get('http://localhost:3000/api/blog', () => {
       contents: [
         {
           id: 1,
+          slug: 'first-blog-content',
           title: 'First Blog Post',
           content: 'This is the first blog post content.',
           date: '2024-07-21',
@@ -15,16 +16,18 @@ const getBlogList = http.get('http://localhost:3000/api/blog', () => {
         {
           id: 2,
           title: 'Second Blog Post',
+          slug: 'first-blog-content',
           content: 'This is the second blog post content.',
-          date: '2024-07-21',
+          date: '2024-07-22',
           thumbnail:
             'https://firebasestorage.googleapis.com/v0/b/my-blog-76a3f.appspot.com/o/3FEF9DDF-3E33-4FF6-93A6-E86D4B046088.JPG?alt=media&token=7080d1f0-927e-4649-85e7-b122a112629f',
         },
         {
           id: 3,
-          title: 'Second Blog Post',
-          content: 'This is the second blog post content.',
-          date: '2024-07-21',
+          title: 'Third Blog Post',
+          content: 'This is the third blog post content.',
+          slug: 'first-blog-content',
+          date: '2024-07-23',
           thumbnail:
             'https://firebasestorage.googleapis.com/v0/b/my-blog-76a3f.appspot.com/o/3FEF9DDF-3E33-4FF6-93A6-E86D4B046088.JPG?alt=media&token=7080d1f0-927e-4649-85e7-b122a112629f',
         },
