c9bae0f24d7c3269cd4c93ccc6531840a95d15ac
minji Kong <minzyaaaaaa@gmail.com>
2024-08-18 22:07:17 +0900

    Feat: #18 블로그글 상세 조회 api 구현

---
+++ src/app/api/blog/[slug]/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { collection, query, Query, where, getDocs } from 'firebase/firestore';
+import { fireStore as db } from '@/firebase/index';
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  let blogListQuery: Query = collection(db, 'blogs');
+
+  if (params.slug) {
+    blogListQuery = query(blogListQuery, where('slug', '==', params.slug));
+  }
+
+  const querySnapshot = await getDocs(blogListQuery);
+
+  if (querySnapshot.empty) {
+    return NextResponse.json(
+      { message: '존재하지 않는 블로그 제목입니다.' },
+      { status: 404 }
+    );
+  }
+
+  const doc = querySnapshot.docs[0];
+  const data = {
+    id: doc.id,
+    ...doc.data(),
+  };
+
+  return NextResponse.json({ message: 'success', data }, { status: 200 });
+}
