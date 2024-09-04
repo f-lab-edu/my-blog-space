cc680c875dcb9ea9dc4a5fbde2991e2e3534602a
minji Kong <minzyaaaaaa@gmail.com>
2024-09-04 23:35:22 +0900

    Refactor: API 공통 처리 함수 구현

--- src/app/api/gallery/[slug]/route.ts
+++ src/app/api/gallery/[slug]/route.ts
@@ -1,38 +1,47 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 import { fireStore as db, FIREBASE_COLLECTION_KEYS } from '@/firebase';
+import { handler, CustomError } from '@/app/api/_lib/handler';
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { slug: string } }
-) {
-  const slug = params.slug;
+type FetchGalleryDetailResponse = {
+  id: string;
+  artist: string;
+  date: string;
+  slug: string;
+  title: string;
+};
 
-  if (!slug)
-    return NextResponse.json(
-      { message: '잘못된 요청 정보입니다.' },
-      { status: 400 }
+const isValidResponse = (data: {
+  [key: string]: string;
+}): data is FetchGalleryDetailResponse => {
+  if (data.id && data.artist && data.date && data.slug && data.title)
+    return true;
+  return false;
+};
+
+export const GET = handler(
+  async (request: NextRequest, { params }: { params: { slug: string } }) => {
+    const slug = params.slug;
+
+    if (!slug) throw new CustomError(400);
+
+    const galleryListQuery = query(
+      collection(db, FIREBASE_COLLECTION_KEYS.GALLERIES),
+      where('slug', '==', params.slug)
     );
 
-  const galleryListQuery = query(
-    collection(db, FIREBASE_COLLECTION_KEYS.GALLERIES),
-    where('slug', '==', params.slug)
-  );
+    const querySnapshot = await getDocs(galleryListQuery);
 
-  const querySnapshot = await getDocs(galleryListQuery);
+    if (querySnapshot.empty) throw new CustomError(404);
 
-  if (querySnapshot.empty) {
-    return NextResponse.json(
-      { message: '존재하지 않는 데이터입니다.' },
-      { status: 404 }
-    );
+    const doc = querySnapshot.docs[0];
+    const data = {
+      id: doc.id,
+      ...doc.data(),
+    };
+
+    if (!isValidResponse(data)) throw new CustomError(500);
+
+    return NextResponse.json({ message: 'success', data }, { status: 200 });
   }
-
-  const doc = querySnapshot.docs[0];
-  const data = {
-    id: doc.id,
-    ...doc.data(),
-  };
-
-  return NextResponse.json({ message: 'success', data }, { status: 200 });
-}
+);
