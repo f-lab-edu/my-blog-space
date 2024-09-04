162b28842fb13f0fb824f652e36b13e2b57f171d
minji Kong <minzyaaaaaa@gmail.com>
2024-09-04 23:28:23 +0900

    Feat: 갤러리 상세 페이지 UI 구현

---
+++ src/app/gallery/[title]/layout.tsx
@@ -0,0 +1,29 @@
+import { fetchGalleryDetail } from '@/features/gallery/apis';
+import { getTimeDifference } from '@/shared/lib/getTimeDifference';
+import Comment from '@/entities/comment/Utterances';
+
+export default async function BlogDetailLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: { title: string };
+}>) {
+  const { date, title, artist } = await fetchGalleryDetail(params.title);
+
+  return (
+    <article>
+      <header className='mb-8'>
+        <h2 className='pb-2 text-center font-bold text-4xl'>{title}</h2>
+        <div className='flex justify-center gap-2 font-light text-sm text-slate-600'>
+          <span>{getTimeDifference(date)}</span>
+          <span>@{artist}</span>
+        </div>
+      </header>
+      {children}
+      <div className='mt-8'>
+        <Comment />
+      </div>
+    </article>
+  );
+}
