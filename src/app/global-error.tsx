cc7ac2a374224b78e4d8e735fe1f1ff0da8f8721
minji Kong <minzyaaaaaa@gmail.com>
2024-09-08 22:14:53 +0900

    Feat: global-error page

---
+++ src/app/global-error.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <html className='content-center h-full'>
+      <body className='text-center'>
+        <p>애플리케이션에 문제가 발생했습니다.</p>
+        {error.digest && (
+          <p className='error-digest'>오류 참조 번호: {error.digest}</p>
+        )}
+        <button className='font-serif border mt-4 p-4' onClick={() => reset()}>
+          Try again
+        </button>
+      </body>
+    </html>
+  );
+}
