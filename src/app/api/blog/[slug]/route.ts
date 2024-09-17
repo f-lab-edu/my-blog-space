3a2d03a72c1c35cfbeba6dd41e1df74ffdc5f413
minji Kong <minzyaaaaaa@gmail.com>
2024-09-18 01:38:08 +0900

    Refactor: 단일 데이터 조회 api 로직 수정

--- src/app/api/blog/[slug]/route.ts
+++ src/app/api/blog/[slug]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import { getDoc, doc } from 'firebase/firestore';
 import { fireStore as db, FIREBASE_COLLECTION_KEYS } from '@/firebase';
 import { handler, CustomError } from '@/app/api/_lib/handler';
 
@@ -9,18 +9,14 @@ export const GET = handler(
 
     if (!slug) throw new CustomError(400);
 
-    const blogListQuery = query(
-      collection(db, FIREBASE_COLLECTION_KEYS.BLOGS),
-      where('slug', '==', slug)
-    );
+    const docRef = doc(db, FIREBASE_COLLECTION_KEYS.BLOGS, slug);
+    const docSnap = await getDoc(docRef);
 
-    const querySnapshot = await getDocs(blogListQuery);
-    if (querySnapshot.empty) throw new CustomError(404);
+    if (!docSnap.exists()) throw new CustomError(404);
 
-    const doc = querySnapshot.docs[0];
     const data = {
-      id: doc.id,
-      ...doc.data(),
+      id: docSnap.id,
+      ...docSnap.data(),
     };
 
     return NextResponse.json({ message: 'success', data }, { status: 200 });
--- src/app/api/gallery/[slug]/route.ts
+++ src/app/api/gallery/[slug]/route.ts
@@ -1,23 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import { getDoc, doc } from 'firebase/firestore';
 import { fireStore as db, FIREBASE_COLLECTION_KEYS } from '@/firebase';
 import { handler, CustomError } from '@/app/api/_lib/handler';
-
-type FetchGalleryDetailResponse = {
-  id: string;
-  artist: string;
-  date: string;
-  slug: string;
-  title: string;
-};
-
-const isValidResponse = (data: {
-  [key: string]: string;
-}): data is FetchGalleryDetailResponse => {
-  if (data.id && data.artist && data.date && data.slug && data.title)
-    return true;
-  return false;
-};
 
 export const GET = handler(
   async (request: NextRequest, { params }: { params: { slug: string } }) => {
@@ -25,21 +9,14 @@ export const GET = handler(
 
     if (!slug) throw new CustomError(400);
 
-    const galleryListQuery = query(
-      collection(db, FIREBASE_COLLECTION_KEYS.GALLERIES),
-      where('slug', '==', params.slug)
-    );
+    const docRef = doc(db, FIREBASE_COLLECTION_KEYS.GALLERIES, slug);
+    const docSnap = await getDoc(docRef);
+    if (!docSnap.exists()) throw new CustomError(404);
 
-    const querySnapshot = await getDocs(galleryListQuery);
-    if (querySnapshot.empty) throw new CustomError(404);
-
-    const doc = querySnapshot.docs[0];
     const data = {
-      id: doc.id,
-      ...doc.data(),
+      id: docSnap.id,
+      ...docSnap.data(),
     };
-
-    if (!isValidResponse(data)) throw new CustomError(500);
 
     return NextResponse.json({ message: 'success', data }, { status: 200 });
   }
